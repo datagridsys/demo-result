ab0dab4e02478383ba6439bfdc07a17340cfcc29
Stephen Quintero <stephen@datagridsys.com>
2017-04-16 04:24:56 +0000

    demo-result:  add v1/v2 for server.js and views/app.js

---
+++ dcon_original/views/v2.app.js
@@ -0,0 +1,58 @@
+var app = angular.module('catsvsdogs', []);
+var socket = io.connect({transports:['polling']});
+
+var bg1 = document.getElementById('background-stats-1');
+var bg2 = document.getElementById('background-stats-2');
+
+app.controller('statsCtrl', function($scope){
+  $scope.aPercent = 50;
+  $scope.bPercent = 50;
+
+  var version = 2
+
+  var updateScores = function(){
+    socket.on('scores', function (json) {
+       data = JSON.parse(json);
+       var a = parseInt(data.a || 0);
+       var b = parseInt(data.b || 0);
+
+       var backendVer = parseInt(data.version || 0);
+       if (backendVer != version) {
+         console.log("reloading")
+         window.location.reload();
+       }
+
+       var percentages = getPercentages(a, b);
+
+       bg1.style.width = percentages.a + "%";
+       bg2.style.width = percentages.b + "%";
+
+       $scope.$apply(function () {
+         $scope.aPercent = percentages.a;
+         $scope.bPercent = percentages.b;
+         $scope.total = a + b;
+       });
+    });
+  };
+
+  var init = function(){
+    document.body.style.opacity=1;
+    updateScores();
+  };
+  socket.on('message',function(data){
+    init();
+  });
+});
+
+function getPercentages(a, b) {
+  var result = {};
+
+  if (a + b > 0) {
+    result.a = Math.round(a / (a + b) * 100);
+    result.b = 100 - result.a;
+  } else {
+    result.a = result.b = 50;
+  }
+
+  return result;
+}
